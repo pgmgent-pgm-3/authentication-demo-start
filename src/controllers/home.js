0e8d5bf09e1f6b1f0f71a65cb53dcb4f04020263
Frederick Roegiers <frederick@rogerthat.be>
2023-03-10 09:56:03 +0100

    wip

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -5,17 +5,8 @@
 import DataSource from "../lib/DataSource.js";
 
 export const home = async (req, res) => {
-  // get the user repository
-  const userRepository = DataSource.getRepository("User");
-
-  // for DEMO, return the first user in the users table
-  const userData = await userRepository.findOne({
-    where: { id: null },
-    relations: ["meta"],
-  });
-
   // render the home page
   res.render("home", {
-    userData,
+    user: req.user,
   });
 };
--- src/middleware/jwtAuth.js
+++ src/middleware/jwtAuth.js
@@ -1,5 +1,5 @@
-import jwt from 'jsonwebtoken';
-import DataSource from '../lib/DataSource.js';
+import jwt from "jsonwebtoken";
+import DataSource from "../lib/DataSource.js";
 
 export const jwtAuth = async (req, res, next) => {
   const token = req.cookies.token;
@@ -9,13 +9,19 @@ export const jwtAuth = async (req, res, next) => {
     const { id } = jwt.verify(token, process.env.TOKEN_SALT);
 
     // get the user out of the database
-    const userRepository = DataSource.getRepository('User');
+    const userRepository = DataSource.getRepository("User");
     const user = await userRepository.findOne({ where: { id } });
+
+    // remove the password from the user object
+    // so we don't send it to the client, ever, ever, ever
+    user.password = "";
+
+    req.user = user;
 
     // go to the next chain
     next();
-  } catch(e) {
-    res.clearCookie('token');
-    return res.redirect('/login');
+  } catch (e) {
+    res.clearCookie("token");
+    return res.redirect("/login");
   }
-}
+};
